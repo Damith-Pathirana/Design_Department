449b1ef2a848f6328ce9009089d7bf794f5848da
Damith Thathsara Pathirana <belo3998@gmail.com>
2026-04-27 07:01:16 +0530

    fix: set basePath and trailingSlash for GitHub Pages sub-directory hosting

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    // Change this to matches your repository name for GitHub Pages sub-directory hosting
+    basePath: '/Design_Department',
     images: {
         unoptimized: true,
-    }
+    },
+    trailingSlash: true,
 };
 
 export default nextConfig;
